2823cb3544b4f7b55d1a1c0c488ddb2e2ab0199f
vubuitien <vubuitien@gmail.com>
2018-01-10 09:36:35 +0700

    first commit

---
+++ js-hide-show.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="lib/js.js"></script>
+</head>
+<body>
+<button onclick="show()" id="button">show</button>
+<button onclick="hide()" id="button">hide</button>
+<div id="myDIV">
+	This is my DIV element.
+</div>
+</body>
+</html>
+
+
