19a4aac01fb9f1f444da26091a750740cf86e4b2
vubuitien <vubuitien@gmail.com>
2018-01-10 11:21:28 +0700

    Update js-hide-show.php

--- js-hide-show.php
+++ js-hide-show.php
@@ -5,8 +5,8 @@
 	<script type="text/javascript" src="lib/js.js"></script>
 </head>
 <body>
-<button onclick="show()" id="button">show</button>
-<button onclick="hide()" id="button">hide</button>
+<button id="button">show</button>
+<button id="button1">hide</button>
 <div id="myDIV">
 	This is my DIV element.
 </div>
